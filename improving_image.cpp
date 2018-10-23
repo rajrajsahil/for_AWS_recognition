ebc6fdf28fbf42b15852bdd9c9eb3aaf22efe061
rajrajsahil <rajsahildumaribhui@gmail.com>
2018-10-23 20:26:16 +0530

    filter for improving images

--- improving_image.cpp
+++ improving_image.cpp
@@ -7,7 +7,7 @@
 using namespace cv;
 using namespace std;
 
-/** @function main */
+
 int main( int argc, char** argv )
 {
   Mat src, src_gray, dst,final;
@@ -15,11 +15,10 @@ int main( int argc, char** argv )
   int scale = 1;
   int delta = 0;
   int ddepth = CV_16S;
-  char* window_name = "Laplace Demo";
 
   int c;
 
-  /// Load an image
+  // Load an image
   src = imread("face.jpg");
 
   if( !src.data )
@@ -28,12 +27,10 @@ int main( int argc, char** argv )
   /// Remove noise by blurring with a Gaussian filter
   GaussianBlur( src, src, Size(7,7), 0, 0, BORDER_DEFAULT );
 
-  /// Convert the image to grayscale
-  // cvtColor( src, src_gray, CV_BGR2GRAY );
   src_gray = src;
 
   /// Create window
-  namedWindow( window_name, CV_WINDOW_AUTOSIZE );
+  namedWindow("improved_image", CV_WINDOW_AUTOSIZE );
 
   /// Apply Laplace function
   Mat abs_dst;
@@ -42,11 +39,12 @@ int main( int argc, char** argv )
   Mat abs_src_gray;
   convertScaleAbs( src_gray, abs_src_gray );
 
+  //Adding original and Laplacian image
   addWeighted( abs_src_gray, 1, abs_dst, 1, 0.0, final);
 
   /// Show what you got
   imwrite("improved.jpg",final);
-  imshow( window_name, final);
+  imshow("improved_image", final);
 
   waitKey(0);
 
